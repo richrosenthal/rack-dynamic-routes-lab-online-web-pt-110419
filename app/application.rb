8465bf220229664cdafd6ac264ebd178a34273e6
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-02-10 15:16:36 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     req = Rack::Request.new(env)
  
     if req.path.match(/items/<ITEM NAME>)
-      @@items.each do |item|
+      @items.each do |item|
         resp.write "#{item.price}\n"
       end
     else
