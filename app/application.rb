cabd4b06845b711d44468a7b7ee31dc412270327
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-02-10 15:15:08 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,19 @@
+class Application
+ 
+  @@items = ["Apples","Carrots","Pears"]
+ 
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+ 
+    if req.path.match(/items/<ITEM NAME>)
+      @@items.each do |item|
+        resp.write "#{item.price}\n"
+      end
+    else
+      resp.write "Path Not Found"
+    end
+ 
+    resp.finish
+  end
+end
